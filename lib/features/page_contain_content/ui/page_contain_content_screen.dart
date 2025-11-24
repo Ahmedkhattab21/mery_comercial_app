3f5d0889c97e973b4cf58a627e998929bd44acb1
Ahmedkhattab21 <khattabahmed723@gmail.com>
2025-11-24 18:20:10 +0300

    profile done

--- lib/features/page_contain_content/ui/page_contain_content_screen.dart
+++ lib/features/page_contain_content/ui/page_contain_content_screen.dart
@@ -1,13 +1,17 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mery_comercial_app/core/utils/app_colors_white_theme.dart';
 import 'package:mery_comercial_app/core/utils/extentions.dart';
 import 'package:mery_comercial_app/core/utils/spacing.dart';
 import 'package:mery_comercial_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mery_comercial_app/features/page_contain_content/logic/page_contain_content_cubit.dart';
+import 'package:mery_comercial_app/features/page_contain_content/logic/page_contain_content_state.dart';
 
 class PageContainContentScreen extends StatelessWidget {
   final String title;
-  const PageContainContentScreen({required this.title,super.key});
+
+  const PageContainContentScreen({required this.title, super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +27,7 @@ class PageContainContentScreen extends StatelessWidget {
         ),
         title: Text(
           title,
-          style: TextStyles.font18WhiteColorW600 ,
+          style: TextStyles.font18WhiteColorW600,
         ),
         backgroundColor: AppColors.greenColor31,
       ),
@@ -48,14 +52,23 @@ class PageContainContentScreen extends StatelessWidget {
                 Expanded(
                   child: Container(
                     width: double.infinity,
-                    padding: EdgeInsets.symmetric(horizontal: 16.w,vertical: 16.h),
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 16.w, vertical: 16.h),
                     decoration: BoxDecoration(
-                      color: AppColors.whiteColor,
-                      borderRadius: BorderRadius.circular(32.r),
-                      border: Border.all(color: AppColors.greyColor44.withValues(alpha: .2))
+                        color: AppColors.whiteColor,
+                        borderRadius: BorderRadius.circular(32.r),
+                        border: Border.all(
+                            color: AppColors.greyColor44.withValues(alpha: .2))
                     ),
                     child: SingleChildScrollView(
-                      child: Text('AppConstant.terms'),
+                      child: BlocBuilder<PageContainContentCubit, PageContainContentState>(
+                        // buildWhen: (previous,current) {
+                        //   return current is
+                        // },
+                        builder: (context, state) {
+                          return Text('AppConstant.terms');
+                        },
+                      ),
                     ),
                   ),
                 ),
